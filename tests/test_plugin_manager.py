999f12ac5e1d7e964fb9092768b690f3bab514d0
BackrndSource <back.rndsource@gmail.com>
2025-02-10 04:43:18 +0100

    Parametrize test for PluginManager

--- tests/test_plugin_manager.py
+++ tests/test_plugin_manager.py
@@ -9,6 +9,8 @@ from pyboy.plugins.window_sdl2 import WindowSDL2
 from pyboy.plugins.window_open_gl import WindowOpenGL
 from pyboy.plugins.window_null import WindowNull
 from pyboy.plugins.debug import Debug
+import pytest
+import os
 
 
 def test_plugin_manager_basics(default_rom):
@@ -24,37 +26,29 @@ def test_plugin_manager_generic_game_wrapper(default_rom):
     assert type(generic_wrapper) == PyBoyGameWrapper
 
 
-def test_plugin_manager_window_sdl2_plugin(default_rom):
-    pyboy = PyBoy(default_rom, window="SDL2", debug=False)
-    window_plugin = pyboy._plugin_manager.window_plugins_enabled[0]
-    assert type(window_plugin) == WindowSDL2
+@pytest.mark.parametrize(
+    "window_arg, window_cls, debug",
+    [
+        ("SDL2", WindowSDL2, True),
+        ("SDL2", WindowSDL2, False),
+        ("OpenGL", WindowOpenGL, True),
+        ("OpenGL", WindowOpenGL, False),
+        ("null", WindowNull, True),
+        ("null", WindowNull, False),
+    ],
+)
+def test_plugin_manager_window_plugin(default_rom, window_arg, window_cls, debug):
+    if os.environ.get("TEST_NO_UI") and window_arg in ["SDL2", "OpenGL"]:
+        pytest.skip("Skipping test, as there is no UI")
 
+    pyboy = PyBoy(default_rom, window=window_arg, debug=debug)
+    window_plugins_enabled = pyboy._plugin_manager.window_plugins_enabled
 
-def test_plugin_manager_window_sdl2_debug_plugin(default_rom):
-    pyboy = PyBoy(default_rom, window="SDL2", debug=True)
-    window_plugins = pyboy._plugin_manager.window_plugins_enabled
-    assert len(window_plugins) == 2
-    assert type(window_plugins[0]) != type(window_plugins[1])
-    for plugin in window_plugins:
-        assert type(plugin) == WindowSDL2 or type(plugin) == Debug
-
-
-def test_plugin_manager_window_opengl_plugin(default_rom):
-    pyboy = PyBoy(default_rom, window="OpenGL", debug=False)
-    window_plugin = pyboy._plugin_manager.window_plugins_enabled[0]
-    assert type(window_plugin) == WindowOpenGL
-
-
-def test_plugin_manager_window_null_plugin(default_rom):
-    pyboy = PyBoy(default_rom, window="null", debug=False)
-    window_plugin = pyboy._plugin_manager.window_plugins_enabled[0]
-    assert type(window_plugin) == WindowNull
-
-
-def test_plugin_manager_window_null_debug_plugin(default_rom):
-    pyboy = PyBoy(default_rom, window="null", debug=True)
-    window_plugins = pyboy._plugin_manager.window_plugins_enabled
-    assert len(window_plugins) == 2
-    assert type(window_plugins[0]) != type(window_plugins[1])
-    for plugin in window_plugins:
-        assert type(plugin) == WindowNull or type(plugin) == Debug
+    if debug:
+        assert len(window_plugins_enabled) == 2
+        assert type(window_plugins_enabled[0]) != type(window_plugins_enabled[1])
+        for plugin in window_plugins_enabled:
+            assert type(plugin) == window_cls or type(plugin) == Debug
+    else:
+        assert len(window_plugins_enabled) == 1
+        assert type(window_plugins_enabled[0]) == window_cls
